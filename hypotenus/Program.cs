0f00055ce02496fdb7df99b769de019d9e43c4fd
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-12 00:32:52 -0400

    se agrega modulo 'hypotenuse'

---
+++ hypotenus/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Enter side A: ");
+double sideA = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Enter side B: ");
+double sideB = Convert.ToDouble(Console.ReadLine());
+
+double resultado = Math.Sqrt((sideA * sideA) + (sideB * sideB));
+
+System.Console.WriteLine("The Hypotenuse is: " + resultado);
+
+Console.ReadKey();

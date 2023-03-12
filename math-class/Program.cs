c84bca20f4b296dce7cb315467559a402259073a
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-11 20:12:56 -0400

    se agrega modulo 'user-input'

---
+++ math-class/Program.cs
@@ -0,0 +1,8 @@
+﻿double x = 3;
+
+double a = Math.Pow(x, 2); // we raise x to the second power
+System.Console.WriteLine("This is {0} to the second power: {1}", x, a);
+
+double numeroCuadrado = Math.Sqrt(x);
+System.Console.WriteLine($"The square root of {x} is " + numeroCuadrado);
+Console.ReadKey();
---
+++ user-input/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What's your name?");
+string name = Console.ReadLine();
+
+Console.WriteLine("What's your age?");
+int age = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Hello " + name);
+System.Console.WriteLine("You are " + age + " years old.");
+
+Console.ReadKey();

11a41c449d8fec690295521534c8e2b54a73910f
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-11 22:43:32 -0400

    se agrega modulo 'random'

---
+++ random/Program.cs
@@ -0,0 +1,19 @@
+﻿Random random = new Random();
+
+int num1 = random.Next(1, 7);
+int num2 = random.Next(1, 7);
+long num3 = random.NextInt64();
+
+// double num random
+double num1Double = random.NextDouble();
+double num2Double = random.NextDouble();
+double num3Double = random.NextDouble();
+
+System.Console.WriteLine("Integer entre 1-7: "+ num1);
+System.Console.WriteLine("Integer entre 1-7: "+ num2);
+System.Console.WriteLine("Long Number NextInt64: " + num3);
+System.Console.WriteLine(num1Double);
+System.Console.WriteLine(num2Double);
+System.Console.WriteLine(num3Double);
+
+Console.ReadKey();

dbde03f2797ce45edeadc596b913a9db92c8a5fd
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-12 23:55:53 -0400

    se agregan loc a modulo 'branching'

--- branching/Program.cs
+++ branching/Program.cs
@@ -1,15 +1,32 @@
 ﻿int numero = 100;
 
 // IF STATEMENT
-if(numero > 100) {
+if (numero > 100)
+{
     Console.WriteLine("Numero Mayor a 100");
 }
-else if (numero == 100) {
+else if (numero == 100)
+{
     Console.WriteLine("El numero es 100");
 }
-else {
+else
+{
     Console.WriteLine("Numero menor a 100");
 }
 
 // TERNARY
-Console.WriteLine((numero > 99) ? "Numero mayor a 99" : "Numero menor a 99" );
+Console.WriteLine((numero > 99) ? "Numero mayor a 99" : "Numero menor a 99");
+
+// SWITCH STATEMENT
+String dia = "Domingo";
+
+switch (dia)
+{
+    case "Lunes":
+        Console.WriteLine("El dia es Lunes");
+        break;
+    case "Domingo":
+        Console.WriteLine("El dia es Domingo");
+        break;
+   
+}

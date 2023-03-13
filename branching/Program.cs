67b669b0de6c7b6658e912f72a7b92bc46c65e20
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-12 21:22:05 -0400

    se agrega modulo 'branching'

---
+++ branching/Program.cs
@@ -0,0 +1,15 @@
+﻿int numero = 100;
+
+// IF STATEMENT
+if(numero > 100) {
+    Console.WriteLine("Numero Mayor a 100");
+}
+else if (numero == 100) {
+    Console.WriteLine("El numero es 100");
+}
+else {
+    Console.WriteLine("Numero menor a 100");
+}
+
+// TERNARY
+Console.WriteLine((numero > 99) ? "Numero mayor a 99" : "Numero menor a 99" );

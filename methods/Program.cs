812dd8698c24d543c7bc59c7c352a8391fce2dda
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-13 21:46:23 -0400

    se agrega modulo 'methods'

---
+++ methods/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program {
+    static void Main(string[] args)
+    {
+    int age = 30;
+    string name = "dave";
+        singHapyBirthday();
+        singHapyBirthdayDos(name, age);
+
+
+        Console.ReadKey();
+    }
+
+    static void singHapyBirthday()
+    {
+        System.Console.WriteLine("Happy Birthday to you");
+        System.Console.WriteLine("Happy Birthday to you");
+    }
+
+    static void singHapyBirthdayDos(string name, int age)
+    {
+        System.Console.WriteLine("Happy Birthday to you");
+        System.Console.WriteLine("Happy Birthday to you");
+        System.Console.WriteLine("Happy Birthday {0}" , name);
+        System.Console.WriteLine("You are {0} years old" , age);
+        System.Console.WriteLine("Happy Birthday to you");
+
+    }
+}

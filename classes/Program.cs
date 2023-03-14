13a1f13ce23df711d04b5429441b42a4df2e0ed0
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-13 22:00:10 -0400

    se agregan loc a modulo 'classes'

--- classes/Program.cs
+++ classes/Program.cs
@@ -5,33 +5,44 @@ namespace classes
 {
     class Program
     {
-                     
-            // aca se crea la clase
-            class Book
+        // aca se crea la clase
+        class Book
         {
             public string title;
             public string author;
             public int yearOfPublication;
         }
+
+        // aca se crea otra clase
+        class Person
+        {
+            public string name;
+            public int age;
+        }
+
         static void Main(string[] args)
         {
-
             // aca se crea un objeto de esa clase
-            Book bookUno = new Book ();
+            Book bookUno = new Book();
             // pupulate his attributes(fields)
             bookUno.title = "Necronomicon";
             bookUno.author = "H.P. Lovecraft";
             bookUno.yearOfPublication = 1921;
 
-
-
             // aca se muestra por consola
             const string bookTitle = "Title: ";
             const string bookAuthor = "Author: ";
-        Console.WriteLine(bookTitle + bookUno.title);
-        Console.WriteLine(bookAuthor + bookUno.author);
-        Console.WriteLine("Year: "+ bookUno.yearOfPublication);
+            Console.WriteLine(bookTitle + bookUno.title);
+            Console.WriteLine(bookAuthor + bookUno.author);
+            Console.WriteLine("Year: " + bookUno.yearOfPublication);
 
+            // se crea un objeto de la clase Person
+            Person personUno = new Person();
+            // populate hus atributes(fields)
+            personUno.name = "Philip";
+            personUno.age= 50;
+            System.Console.WriteLine(personUno.name);
+            System.Console.WriteLine(personUno.age);
         }
     }
 }
